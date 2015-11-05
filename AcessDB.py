b26581535c1b6516f8110ae15daa41d97503b5c0
chengjia-su <chengjia.su@gmail.com>
2015-11-05 12:31:15 +0800

    v1.0
    part accessDB module from Hass

---
+++ AcessDB.py
@@ -0,0 +1,77 @@
+import logging
+
+import MySQLdb, MySQLdb.cursors
+
+class AcessDB(object):
+
+    def __init__ (self):
+
+        try:
+            self.dbconn = MySQLdb.connect(  host = config.get("mysql", "mysql_ip"),
+                                        user = config.get("mysql", "mysql_username"),
+                                        passwd = config.get("mysql", "mysql_password"),
+                                        db = "hass",
+                                    )
+        except MySQLdb.Error, e:
+            logging.error("Hass AccessDB - connect to database failed (MySQL Error: %s)", str(e))
+            print "MySQL Error: %s" % str(e)
+            sys.exit(1)
+
+        self.db = self.dbconn.cursor(cursorclass = MySQLdb.cursors.DictCursor)
+        try:
+            self.db.execute("SET sql_notes = 0;")
+            self.db.execute("""
+                            CREATE TABLE IF NOT EXISTS ha_cluster 
+                            (
+                            cluster_uuid char(36),
+                            cluster_name char(18),
+                            PRIMARY KEY(cluster_uuid)
+                            );
+                            """)
+            self.db.execute("""
+                            CREATE TABLE IF NOT EXISTS ha_node 
+                            (
+                            node_id MEDIUMINT NOT NULL AUTO_INCREMENT,
+                            node_name char(18),
+                            below_cluster char(36),
+                            PRIMARY KEY(node_id),
+                            FOREIGN KEY(below_cluster)
+                                REFERENCES ha_cluster(cluster_uuid)
+                                ON DELETE CASCADE
+                            );
+                            """)
+        except MySQLdb.Error, e:
+            logging.error("Hass AccessDB - Read data failed (MySQL Error: %s)", str(e))
+            print "MySQL Error: %s" % str(e)
+            
+    def readDB(self):
+        self.db.execute("SELECT * FROM ha_cluster;")
+        ha_cluster_date = self.db.fetchall()
+        for cluster in ha_cluster_date:
+            nodeList = []
+            self.db.execute("SELECT * FROM ha_node WHERE below_cluster = "+cluster["cluster_uuid"])
+            ha_node_date = self.db.fetchall()
+            for node in ha_node_date:
+                nodeList.append(node[node_name])
+            newCluster = Cluster(uuid = cluster["cluster_uuid"], name = cluster["cluster_name"])
+            recovery.clusterList[cluster["cluster_uuid"]] = newCluster
+            recovery.addNode(cluster["cluster_uuid"], nodeList)
+            
+    def writeDB(self, dbname, data):
+        if dbname == "ha_cluster":
+            format = "INSERT INTO ha_cluster (cluster_uuid,cluster_name) VALUES (%(uuid)s, %(name)s);"
+        else:
+            format = "INSERT INTO ha_node (node_name,below_cluster) VALUES (%(name)s, %(cluster)s);"
+        try:
+            self.db.execute(format, data)
+        except:
+            logging.error("Hass AccessDB - write data to DB Failed (MySQL Error: %s)", str(e))
+            print "MySQL Error: %s" % str(e)
+            raise
+    
+    def deleteData(self, sql, data):
+        self.db.execute(sql, data)
+    
+    def closeDB(self):
+        self.db.close()
+        self.dbconn.close()
--- Hass.py
+++ Hass.py
@@ -21,6 +21,7 @@ import sys
 
 from Recovery import Recovery
 from Recovery import Cluster
+from AcessDB import AcessDB
 
 config = ConfigParser.RawConfigParser()
 config.read('hass.conf')
@@ -107,7 +108,7 @@ class Hass (object):
         result = recovery.deleteCluster(uuid)
         
         db = AccessDB()
-        db.deleteData("DELETE FROM ha_cluster WHERE cluster_uuid = '%s'" % uuid)
+        db.deleteData("DELETE FROM ha_cluster WHERE cluster_uuid = %s", uuid)
         return result
         
     def addNode(self, clusterId, nodeList):
@@ -125,7 +126,7 @@ class Hass (object):
     def deleteNode(self, clusterId, nodename):
         result = recovery.deleteNode(clusterId, nodename)
         db = AccessDB()
-        db.deleteData("DELETE FROM ha_node WHERE node_name = '%s' && below_cluster = '%s'" % (nodename, clusterId))
+        db.deleteData("DELETE FROM ha_node WHERE node_name = %s && below_cluster = %s", (nodename, clusterId))
         return result
     #def showCluster(self, uuid):
         
@@ -133,79 +134,7 @@ class Hass (object):
 
 
        
-class AcessDB(object):
 
-    def __init__ (self):
-        import MySQLdb, MySQLdb.cursors
-        try:
-            self.dbconn = MySQLdb.connect(  host = config.get("mysql", "mysql_ip"),
-                                        user = config.get("mysql", "mysql_username"),
-                                        passwd = config.get("mysql", "mysql_password"),
-                                        db = "hass",
-                                    )
-        except MySQLdb.Error, e:
-            logging.error("Hass AccessDB - connect to database failed (MySQL Error: %s)", str(e))
-            print "MySQL Error: %s" % str(e)
-            sys.exit(1)
-
-        self.db = self.dbconn.cursor(cursorclass = MySQLdb.cursors.DictCursor)
-        try:
-            self.db.execute("SET sql_notes = 0;")
-            self.db.execute("""
-                            CREATE TABLE IF NOT EXISTS ha_cluster 
-                            (
-                            cluster_uuid char(36),
-                            cluster_name char(18),
-                            PRIMARY KEY(cluster_uuid)
-                            );
-                            """)
-            self.db.execute("""
-                            CREATE TABLE IF NOT EXISTS ha_node 
-                            (
-                            node_id MEDIUMINT NOT NULL AUTO_INCREMENT,
-                            node_name char(18),
-                            below_cluster char(36),
-                            PRIMARY KEY(node_id),
-                            FOREIGN KEY(below_cluster)
-                                REFERENCES ha_cluster(cluster_uuid)
-                                ON DELETE CASCADE
-                            );
-                            """)
-        except MySQLdb.Error, e:
-            logging.error("Hass AccessDB - Read data failed (MySQL Error: %s)", str(e))
-            print "MySQL Error: %s" % str(e)
-            
-    def readDB(self):
-        self.db.execute("SELECT * FROM ha_cluster;")
-        ha_cluster_date = self.db.fetchall()
-        for cluster in ha_cluster_date:
-            nodeList = []
-            self.db.execute("SELECT * FROM ha_node WHERE below_cluster = "+cluster["cluster_uuid"])
-            ha_node_date = self.db.fetchall()
-            for node in ha_node_date:
-                nodeList.append(node[node_name])
-            newCluster = Cluster(uuid = cluster["cluster_uuid"], name = cluster["cluster_name"])
-            recovery.clusterList[cluster["cluster_uuid"]] = newCluster
-            recovery.addNode(cluster["cluster_uuid"], nodeList)
-            
-    def writeDB(self, dbname, data):
-        if dbname == "ha_cluster":
-            format = "INSERT INTO ha_cluster (cluster_uuid,cluster_name) VALUES (%(uuid)s, %(name)s);"
-        else:
-            format = "INSERT INTO ha_node (node_name,below_cluster) VALUES (%(name)s, %(cluster)s);"
-        try:
-            self.db.execute(format, data)
-        except:
-            logging.error("Hass AccessDB - write data to DB Failed (MySQL Error: %s)", str(e))
-            print "MySQL Error: %s" % str(e)
-            raise
-    
-    def deleteData(self, sql):
-        self.db.execute(sql)
-    
-    def closeDB(self):
-        self.db.close()
-        self.dbconn.close()
     
 def main():
     
